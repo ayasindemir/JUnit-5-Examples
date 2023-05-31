dcdf9b80142487857e03d0d6d6cbfd7a36e1ee90
ayasin.demir <ayasindemir@gmail.com>
2023-05-31 09:12:33 +0300

    @Disabled

---
+++ test/org/ysn/test/DisabledAnnotation.java
@@ -0,0 +1,24 @@
+package org.ysn.test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class DisabledAnnotation {
+
+	@Test
+	void test1() {
+		System.out.println("test1...");
+	}
+
+	@Test
+	@Disabled
+	void test2() {
+		System.out.println("this test method is disabled");
+	}
+
+	@Test
+	void test3() {
+		System.out.println("test3...");
+	}
+
+}
