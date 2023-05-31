498d2c2afe365532c9f135affd0f3d8e7d355950
ayasin.demir <ayasindemir@gmail.com>
2023-05-31 09:14:09 +0300

    @Test

---
+++ test/org/ysn/test/TestAnnotation.java
@@ -0,0 +1,23 @@
+package org.ysn.test;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class TestAnnotation {
+
+	@Test
+	void test1() {
+		System.out.println("test1...");
+	}
+
+	@Test
+	void test2() {
+		System.out.println("test2...");
+	}
+
+	@Test
+	void test3() {
+		System.out.println("test3...");
+	}
+
+}
